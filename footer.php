cb98e06d7e82335584bf42af4507d74b56f45c1d
abdulsamad245 <abdulsamadbalogun25@gmail.com>
2022-04-12 17:53:16 +0100

    first commit

---
+++ footer.php
@@ -0,0 +1,5 @@
+<?php
+    echo "<p>Copywright &copy; 1999-" .date("Y")."PHP MasterClass</p>"
+?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,8 @@
+<?php
+    $title = 'Home';
+    require_once 'includes/header.php';
+?>
+<h1>Registration for IT Conference</h1>
+<?php
+    require_once 'includes/footer.php';
+?>
